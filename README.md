7b88a68b15c49c74862cfcb476603d203221c7cf
Nasudonguri <92015563+nasudonguri@users.noreply.github.com>
2023-02-09 17:15:35 +0900

    Update

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Nasudonguri CSS
+# How to use
+* Insert the following code in the head tag.
+```
+<script src=""></script>
+```
+
+* Insert the following code in the body tag.
+```
+<noscript>
+  <h1>Please enable JavaScript.<br>JavaScriptを有効化してください。</h1>
+</noscript>
+```
+
+This is made by Nasudonguri.
+(C) Nasudonguri 2023
+MIT License
--- script.js
+++ script.js
@@ -1,21 +1,53 @@
 let cdnList = {
   js: [
+    'https://cdn.jsdelivr.net/npm/muicss/dist/js/mui.min.js',
+    'https://cdn.jsdelivr.net/npm/ua-parser-js@latest/dist/ua-parser.min.js',
     'https://cdn.jsdelivr.net/npm/@twemoji/api@latest/dist/twemoji.min.js'
   ],
   css: [
+    'https://cdn.jsdelivr.net/npm/muicss/dist/css/mui.min.css',
+    'https://fonts.googleapis.com/icon?family=Material+Icons'
   ]
 };
 
-window.onload = () => {
+let pageConfig = {};
+
+let pageList = {};
+
+let headerHTML = ``;
+let footerHTML = ``;
+
+document.title = 'Loading';
+
+document.head.onload = () => {
+  // Add JavaScript CDN
   for(path of cdnList.js){
     let jsTag = document.createElement('script');
     jsTag.src = path;
     document.head.appendChild(jsTag);
-  }
+  };
+
+  // Add CSS CDN
   for(path of cdnList.css){
     let cssTag = document.createElement('link');
     cssTag.rel = 'stylesheet';
     cssTag.href = path;
     document.head.appendChild(cssTag);
-  }
-}
+  };
+};
+
+document.body.onload = () => {
+  // Add header
+  let header = document.createElement('header');
+  header.innerHTML = headerHTML;
+  document.body.prepend(header);
+
+  // Add footer
+  let footer = document.createElement('footer');
+  footer.innerHTML = footerHTML;
+  document.body.prepend(footer);
+};
+
+window.onload = () => {
+  document.title = pageConfig.title;
+};
